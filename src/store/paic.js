787255e8f4b497f5dd4cceedf18fbb4459790e30
yeweiping001 <ex_yeweiping001@163.com>
2018-08-14 15:11:56 +0800

    url

--- src/store/paic.js
+++ src/store/paic.js
@@ -1,6 +1,5 @@
 //test
 const domain = 'http://peimc-smp-stg.pa18.com';
-//const domain = 'http://localhost:8089';
 
 export const queryEmpInfo = domain+'/peimc_smp/rest/loveLetter/queryEmpInfo';
 export const sendLoveLetter = domain+'/peimc_smp/rest/loveLetter/sendLoveLetter';
--- src/store/types.js
+++ src/store/types.js
@@ -1,5 +1,4 @@
-//const domain = 'http://103.28.215.253:10662';
-const domain = 'http://localhost:8089';
+const domain = 'http://103.28.215.253:10662';
 export const getAcrostic = domain+'/api/v1/poem';
 export const getGender = domain+'/api/v1/gender';
 export const getTheme = domain+'/api/v1/themes';
