9222721362fd4926858c3b1ccea87a74385179eb
07benson <benson.up@gmail.com>
2018-08-16 17:56:59 +0800

    fix emoji bug

--- src/components/firstPage.vue
+++ src/components/firstPage.vue
@@ -154,6 +154,41 @@
           }
         });
       },
+
+      isEmojiCharacter(substring) {
+        for ( var i = 0; i < substring.length; i++) {
+        var hs = substring.charCodeAt(i);
+        if (0xd800 <= hs && hs <= 0xdbff) {
+          if (substring.length > 1) {
+            var ls = substring.charCodeAt(i + 1);
+            var uc = ((hs - 0xd800) * 0x400) + (ls - 0xdc00) + 0x10000;
+            if (0x1d000 <= uc && uc <= 0x1f77f) {
+              return true;
+            }
+          }
+        } else if (substring.length > 1) {
+          var ls = substring.charCodeAt(i + 1);
+          if (ls == 0x20e3) {
+            return true;
+          }
+        } else {
+          if (0x2100 <= hs && hs <= 0x27ff) {
+            return true;
+          } else if (0x2B05 <= hs && hs <= 0x2b07) {
+            return true;
+          } else if (0x2934 <= hs && hs <= 0x2935) {
+            return true;
+          } else if (0x3297 <= hs && hs <= 0x3299) {
+            return true;
+          } else if (hs == 0xa9 || hs == 0xae || hs == 0x303d || hs == 0x3030
+            || hs == 0x2b55 || hs == 0x2b1c || hs == 0x2b1b
+            || hs == 0x2b50) {
+            return true;
+            }
+        }   
+      }
+    },
+
       getKeywordList(){
         
         if (!this.isChn(this.userName) || this.strLen(this.userName)<4) {
@@ -305,7 +340,14 @@
         var self = this;
         // 要判断输入两个字以上再发请求，只支持中文
         var ischn = this.isChn(this.userName),
-            len = this.strLen(this.userName);
+            len = this.strLen(this.userName),
+            isemoji = this.isEmojiCharacter(this.userName);
+
+        if (isemoji) {
+          this.alert('暂不支持emoji😄');
+          this.userName = '';
+          return
+        }
 
         if (len>=4 && ischn && len<=8) {
           this.$get(types.getGender, {name: this.userName}).then((response => {
